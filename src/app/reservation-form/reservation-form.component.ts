614a60fada51b7816fc4d01a77992a2130343b24
naaji-navas <najidnavas2000@gmail.com>
2024-01-30 10:22:18 +0530

    added the form for the hotel app

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit{
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate : ['', Validators.required],
+      checkOutDate : ['', Validators.required],
+    });
+  }
+  constructor(private formBuilder:FormBuilder) {
+    
+   }
+  reservationForm: FormGroup = new FormGroup({
 
+  });
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      console.log('form is valid');
+    }
+  }
 }
